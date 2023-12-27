a8d957430f8360207a9da7af829264f8f13e3196
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-27 17:20:01 +0100

    Updated main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 17:33:51 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/18 21:34:35 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/27 17:08:24 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,19 @@
 #include "AAnimal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
-#include "WrongAnimal.hpp"
-#include "WrongCat.hpp"
 
 int main(void)
 {
 	const AAnimal *j = new Dog();
 	const AAnimal *i = new Cat();
-	const WrongAnimal *wrongMeta = new WrongAnimal();
-	const WrongCat *wrongI = new WrongCat();
 
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); // will output the cat sound!
+	i->makeSound();
 	j->makeSound();
 
 	delete i;
 	delete j;
-
-	std::cout << wrongI->getType() << " " << std::endl;
-	wrongI->makeSound(); // will output the wrong cat sound!
-	wrongMeta->makeSound();
-
-	delete wrongI;
-	delete wrongMeta;
 
 	// delete new AAnimal(); // this will not compile
 
