978b94402bef9e5b95be24d4a17a8ae5623f29f3
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-27 17:19:17 +0100

    Updated AAnimal

--- ex02/AAnimal.hpp
+++ ex02/AAnimal.hpp
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 14:38:16 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/18 21:10:39 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/23 19:29:58 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ public:
 
 	AAnimal &operator=(AAnimal const &rhs);
 
-	std::string getType(void) const;
+	std::string const &getType(void) const;
 	virtual void makeSound(void) const = 0;
 
 protected:
