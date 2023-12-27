104c9c7049b9e6815fbc27ed620c6be758c7d2dd
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-27 17:19:58 +0100

    Updated WrongCat

--- ex02/WrongCat.cpp
+++ ex02/WrongCat.cpp
@@ -6,25 +6,25 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 17:33:12 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/18 21:26:42 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/23 19:43:20 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongCat.hpp"
 
-WrongCat::WrongCat(void) : WrongAnimal()
+WrongCat::WrongCat(void) : WrongAnimal(), brain(0)
 {
+	std::cout << "WrongCat default constructor called" << std::endl;
 	this->type = "WrongCat";
 	this->brain = new Brain();
-	std::cout << "WrongCat default constructor called" << std::endl;
 
 	return;
 }
 
-WrongCat::WrongCat(WrongCat const &src) : WrongAnimal(src)
+WrongCat::WrongCat(WrongCat const &src) : WrongAnimal(src), brain(0)
 {
-	*this = src;
 	std::cout << "WrongCat copy constructor called" << std::endl;
+	this->brain = src.brain;
 
 	return;
 }
@@ -42,7 +42,7 @@ WrongCat &WrongCat::operator=(WrongCat const &rhs)
 	WrongAnimal::operator=(rhs);
 
 	if (this != &rhs)
-		brain = rhs.brain;
+		this->brain = rhs.brain;
 
 	return *this;
 }
